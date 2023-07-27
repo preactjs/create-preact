99737870d9a1e3ab6a54d0b66e604232853956f2
jdecroock <decroockjovi@gmail.com>
2023-07-27 19:06:32 +0200

    ssg template

---
+++ templates/ssr/renderer/_default.page.server.jsx
@@ -0,0 +1,44 @@
+export { render }
+// See https://vite-plugin-ssr.com/data-fetching
+export const passToClient = ['pageProps', 'urlPathname']
+
+import { renderToString } from 'preact-render-to-string'
+import { PageShell } from './PageShell'
+import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr/server'
+
+async function render(pageContext) {
+	const { Page, pageProps } = pageContext
+	// This render() hook only supports SSR, see https://vite-plugin-ssr.com/render-modes for how to modify render() to support SPA
+	if (!Page) throw new Error('My render() hook expects pageContext.Page to be defined')
+	const pageHtml = renderToString(
+		<PageShell pageContext={pageContext}>
+			<Page {...pageProps} />
+		</PageShell>
+	)
+
+	// See https://vite-plugin-ssr.com/head
+	const { documentProps } = pageContext.exports
+	const title = (documentProps && documentProps.title) || 'Vite SSR app'
+	const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
+
+	const documentHtml = escapeInject`<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8" />
+			<link rel="icon" href="/vite.svg" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="description" content="${desc}" />
+			<title>${title}</title>
+		</head>
+		<body>
+			<div id="app">${dangerouslySkipEscape(pageHtml)}</div>
+		</body>
+		</html>`
+
+  	return {
+		documentHtml,
+		pageContext: {
+		// We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
+		}
+ 	 }
+}
---
+++ templates/ssr/renderer/usePageContext.js
@@ -0,0 +1,19 @@
+// `usePageContext` allows us to access `pageContext` in any Preact component.
+// See https://vite-plugin-ssr.com/pageContext-anywhere
+
+import { createContext } from 'preact'
+import { useContext } from 'preact/hooks'
+
+export { PageContextProvider }
+export { usePageContext }
+
+const Context = createContext(undefined)
+
+function PageContextProvider({ pageContext, children }) {
+  return <Context.Provider value={pageContext}>{children}</Context.Provider>
+}
+
+function usePageContext() {
+  const pageContext = useContext(Context)
+  return pageContext
+}
