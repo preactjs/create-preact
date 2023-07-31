3d4351dcb404376e0c5a344b83edb2919fdf0a45
Ryan Christian <rchristian@ryanchristian.dev>
2023-07-30 22:47:46 -0500

    refactor: Restyle router template

--- templates/config/router/index.jsx
+++ templates/config/router/index.jsx
@@ -1,5 +1,7 @@
 import { render } from 'preact';
 import { LocationProvider, Router, Route } from 'preact-iso';
+
+import { Header } from './components/Header.jsx';
 import { Home } from './pages/Home.jsx';
 import { NotFound } from './pages/_404.jsx';
 import './style.css';
@@ -7,10 +9,13 @@ import './style.css';
 export function App() {
 	return (
 		<LocationProvider>
-			<Router>
-				<Route path="/" component={Home} />
-				<Route default component={NotFound} />
-			</Router>
+			<Header />
+			<main>
+				<Router>
+					<Route path="/" component={Home} />
+					<Route default component={NotFound} />
+				</Router>
+			</main>
 		</LocationProvider>
 	);
 }
