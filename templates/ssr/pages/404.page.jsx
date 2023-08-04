6bd954516d57cc7f4f2464716252c20b9983e4b4
Ryan Christian <rchristian@ryanchristian.dev>
2023-08-04 00:48:18 -0500

    refactor: Restyle SSR template w/ routing

---
+++ templates/ssr/pages/404.page.jsx
@@ -0,0 +1,8 @@
+export function Page() {
+	return (
+		<section>
+			<h1>404: Not Found</h1>
+			<p>It's gone :(</p>
+		</section>
+	);
+}
--- templates/ssr/pages/index/index.page.jsx
+++ templates/ssr/pages/index/index.page.jsx
@@ -1,4 +1,4 @@
-import preactLogo from './assets/preact.svg';
+import preactLogo from '../assets/preact.svg';
 import './style.css';
 
 export function Page() {
